be2c4d6b2c045c0567869beb66970ddc804a22e2
luffydjango2424 <luffydjango2424@gmail.com>
2025-07-02 21:04:44 +0100

    Update settings.py

--- bybit/bybit_project/settings.py
+++ bybit/bybit_project/settings.py
@@ -19,7 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core.apps.CoreConfig',  # Your app
+    'bybit.core.apps.CoreConfig',  # Your app
 ]
 
 MIDDLEWARE = [
