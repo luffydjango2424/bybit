a7c988f8ac3341cd891508a4418d6839f9d0d5f4
luffydjango2424 <luffydjango2424@gmail.com>
2025-07-02 22:59:51 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,6 +10,11 @@ python manage.py collectstatic --no-input
 # Apply migrations
 python manage.py migrate
 
+# Create superuser using the new script 
+python create_superuser.py
+
+
+
 # Create superuser if it doesn't exist
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); \
       if not User.objects.filter(username=os.getenv('DJANGO_SUPERUSER_USERNAME')).exists(): \
