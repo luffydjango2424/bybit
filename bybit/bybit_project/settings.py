99fb9abd140fa87b041faee7ba04045647927550
luffydjango2424 <luffydjango2424@gmail.com>
2025-07-02 22:42:13 +0100

    Update settings.py

--- bybit/bybit_project/settings.py
+++ bybit/bybit_project/settings.py
@@ -71,11 +71,11 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 AUTHENTICATION_BACKENDS = [
-    'core.auth_backend.EmailBackend',
+    'bybit.core.auth_backend.EmailBackend',
     'django.contrib.auth.backends.ModelBackend',
 ]
 
-AUTH_USER_MODEL = 'core.CustomUser'
+AUTH_USER_MODEL = 'bybit.core.CustomUser'
 
 # Internationalization
 LANGUAGE_CODE = 'en-us'
