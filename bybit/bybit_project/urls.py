417c63743104e25bc3dd3065384a977e3c7b2047
luffydjango2424 <luffydjango2424@gmail.com>
2025-07-02 21:15:02 +0100

    Update urls.py

--- bybit/bybit_project/urls.py
+++ bybit/bybit_project/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),  # Add auth URLs
-    path('', include('core.urls')),  # Add core app URLs
+    path('', include('bybit.core.urls')),  # Add core app URLs
 ]
